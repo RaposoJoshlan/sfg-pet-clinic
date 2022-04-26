94d4c1c6b1a842ef7605a0f7590a262fa7a8e167
RaposoJoshlan <raposojoshlan2000@outlook.com>
2022-04-26 14:21:31 +0100

    implemented find owner. closes #54

--- pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,12 +1,18 @@
 package com.springframework.sfgpetclinic.controllers;
 
+import com.springframework.sfgpetclinic.model.Owner;
 import com.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @RequestMapping("/owners")
 @Controller
@@ -18,15 +24,23 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners(Model model) {
-        model.addAttribute("owners", ownerService.findAll());
-        return "owners/index";
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
     }
 
+//    @RequestMapping({"", "/", "/index", "/index.html"})
+//    public String listOwners(Model model) {
+//        model.addAttribute("owners", ownerService.findAll());
+//        return "owners/index";
+//    }
+
     @RequestMapping({"/find"})
-    public String findOwner() {
-        return "notImplemented";
+    public String findOwner(Model model) {
+
+        model.addAttribute("owner", Owner.builder().build());
+
+        return "owners/findOwners";
     }
 
     @GetMapping({"/{ownerId}"})
@@ -36,6 +50,26 @@ public class OwnerController {
         mav.addObject(ownerService.findById(ownerId));
 
         return mav;
+    }
+
+    @GetMapping
+    public String processFindForm(Owner owner, BindingResult result, Model model) {
+        if (owner.getLastName() == null) {
+            owner.setLastName("");
+        }
+
+        List<Owner> results = ownerService.findAllByLastNameLike(owner.getLastName());
+
+        if (results.isEmpty()) {
+            result.rejectValue("lastName", "not found", "not found");
+            return "owners/findOwners";
+        } else if (results.size() == 1) {
+            owner = results.get(0);
+            return "redirect:/owners/" + owner.getId();
+        } else {
+            model.addAttribute("selections", results);
+            return "owners/ownerList";
+        }
     }
 
 
